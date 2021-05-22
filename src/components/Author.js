75e64ddee94ab5a1bbc9ebba6c9dd472dad580d2
sharif7761 <sharif7761@gmail.com>
2021-05-22 16:30:50 +0600

    author component css in jsx

--- src/components/Author.js
+++ src/components/Author.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Author = () => {
     return (
-        <h5>Amelia Hepworth</h5>
+        <h5 style={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Amelia Hepworth</h5>
     );
 };
 
