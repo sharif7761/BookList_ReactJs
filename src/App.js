4a382d1cb81908e1a3f201e7cb457f249168f433
sharif7761 <sharif7761@gmail.com>
2021-05-23 01:45:48 +0600

    children props

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
             <Book img="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
                   title="I Love You to the Moon and Back"
                   author="Amelia Hepworth">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s,</p>
             </Book>
             <Book img="https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg"
                   title="Oh, the Places You'll Go!  "
--- src/components/Book.js
+++ src/components/Book.js
@@ -3,13 +3,14 @@ import Image from "./Image";
 import Title from "./Title";
 import Author from "./Author";
 
-const Book = ({img, title, author}) => {
+const Book = ({img, title, author, children}) => {
     // const {img, title, author} = props
     return (
         <article className='book'>
             <Image img={img} />
             <Title title={title} />
             <Author author={author} />
+            {children}
         </article>
     );
 };
