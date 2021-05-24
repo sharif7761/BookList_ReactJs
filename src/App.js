1705ecde6d61ea85c27f8699266f722a6c34d2f9
sharif7761 <sharif7761@gmail.com>
2021-05-24 21:24:41 +0600

    map method and show book list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Book from "./components/Book";
 import './index.css'
+import data from './data'
 
 function App() {
     return (
         <section className="booklist">
-            <Book img="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-                  title="I Love You to the Moon and Back"
-                  author="Amelia Hepworth">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s,</p>
-            </Book>
-            <Book img="https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg"
-                  title="Oh, the Places You'll Go!  "
-                  author="Dr. Seuss"/>
+            {
+               data.map((book)=>{
+                   return (
+                       <Book img={book.img}
+                             title={book.title}
+                             author={book.author}>
+                       </Book>
+                   )
+               })
+            }
+
         </section>
     );
 }
---
+++ src/data.js
@@ -0,0 +1,18 @@
+import Book from "./components/Book";
+
+export default [
+    {
+        id: 1,
+        title: 'I Love You to the Moon and Back',
+        author: 'Amelia Hepworth',
+        img:
+            'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    },
+    {
+        id: 2,
+        title: 'Oh, the Places You\'ll Go!  ',
+        author: 'Dr. Seuss',
+        img:
+            'https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg',
+    },
+];
